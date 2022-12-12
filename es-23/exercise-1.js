4972f7801bedd4d83046b94c37f81b9cbd72d6ef
Moreno Maroccia <MorenoMaroccia@g,mail.com>
2022-12-12 16:03:51 +0100

    esercizio9

--- es-23/exercise-1.js
+++ es-23/exercise-1.js
@@ -3,7 +3,12 @@ const person = {
   lastName: 'Rossi',
   age: 25
 }
+let keys =Object.keys(person)
+let values =Object.values(person)
+let length =Object.keys(person).length
 
-const person2 = Object.assign({}, person )
-console.log(person2)
+for(let i=0 ; i< length; i++) {
+  console.log(keys[i]+ " : "+ values[i])
+}
+
 // Print values of person using Object.keys
