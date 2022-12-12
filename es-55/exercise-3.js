64684c27fdc5fa990b0d45f7d3f32cb0aafd7597
Moreno Maroccia <MorenoMaroccia@g,mail.com>
2022-12-12 16:35:00 +0100

    esercizio13

--- es-55/exercise-3.js
+++ es-55/exercise-3.js
@@ -1,25 +1,11 @@
-// function calculateAverageAge(persons) {
-//   let adult = persons.map(, reduce((a,b,) =>a.age + b.age)
-//   );
-//   return adult
-// }
-
-// const persons = [
-//   { name: 'Paul', age: 16 },
-//   { name: 'George', age: 17 },
-//   { name: 'Lucas', age: 21 },
-//   { name: 'Marco', age: 32 },
-//   { name: 'Peter', age: 18 },
-//   { name: 'Carl', age: 13 },
-//   { name: 'Simon', age: 24 },
-//   { name: 'Mark', age: 15 },
-//   { name: 'Sandra', age: 34 },
-//   { name: 'Alice', age: 28 }
-// ];
-
-// const average = calculateAverageAge(persons);
-// console.log(persons);
-// console.log(average);
+function calculateAverageAge(persons) {
+  let reduced =persons.reduce ((a,b) => b.age + a,0 )
+  let length=persons.length
+  let totalAge= Math.round(reduced/length)
+   
+  return totalAge
+  
+}
 
 const persons = [
   { name: 'Paul', age: 16 },
@@ -33,16 +19,8 @@ const persons = [
   { name: 'Sandra', age: 34 },
   { name: 'Alice', age: 28 }
 ];
-let sum=0
-let mediaEta;
-function calculateAverageAge(persons1) {
-  for (let i = 0; i < persons1.length; i++) {
 
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
 
-      sum=persons1[i].age+sum;
-   mediaEta=sum/persons1.length
-  }
-  console.log(Math.round(mediaEta))
-}
-calculateAverageAge(persons);
-console.log(calculateAverageAge);
