601827c052f63c838fea54f01965932dcc8d5b42
Moreno Maroccia <MorenoMaroccia@g,mail.com>
2022-12-21 11:02:33 +0100

    esercizio26

--- es-65/exercise-1.js
+++ es-65/exercise-1.js
@@ -1,5 +1,6 @@
-function sum(num1, num2, num3, num4, num5) {
-    return num1 + num2 + num3 + num4 + num5; 
+function sum(...num) {
+    let sum = num.reduce((a,b) => a+b)
+    return sum; 
 }
 
 console.log(sum(1, 2, 3, 4, 5));
