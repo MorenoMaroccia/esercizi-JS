7ba1aff0d66f219b27bd9c82bd9641fad76af5a4
Moreno Maroccia <MorenoMaroccia@g,mail.com>
2023-01-10 12:06:16 +0100

    esercizio27

--- es-66/exercise-2.js
+++ es-66/exercise-2.js
@@ -1,15 +1,26 @@
-const person = {
-  id: 1,
-  firstName: 'Mario',
-  lastName: 'Rossi',
-  age: 25
-};
+// const person = {
+//   id: 1,
+//   firstName: 'Mario',
+//   lastName: 'Rossi',
+//   age: 25
+// };
 
-const id = person.id;
-const personInfo = {
-  firstName: person.firstName,
-  lastName: person.lastName,
-  age: person.age
-};
+// const id = person.id;
+// const personInfo = {
+//   firstName: person.firstName,
+//   lastName: person.lastName,
+//   age: person.age
+// };
 
-console.log(id, personInfo);
+// console.log(id, personInfo);
+
+
+
+function person(id, ...personInfo) {
+
+ personInfo = { firstName:firstName, lastName:lastName, age:age}
+
+  console.log( id, personInfo ); 
+}
+
+person(1, firstName="Mario", lastName="Rossi", age=25);
