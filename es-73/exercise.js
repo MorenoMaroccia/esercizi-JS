644ba45dc1855892b16d09afb83d01390916cc6a
Moreno Maroccia <MorenoMaroccia@g,mail.com>
2023-01-11 11:22:30 +0100

    esercizio34

---
+++ es-73/exercise.js
@@ -0,0 +1,9 @@
+
+
+function printAsyncName(callBack, name1='name') {
+    setTimeout(() => console.log(callBack), 1000);
+    setTimeout(() => console.log(name1),2000);
+}
+
+printAsyncName(('Hello'), ('Mario'))
+
